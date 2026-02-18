30bed13ad23fd5f488d805db878bc528bdf25906
Simon Carstensen <simonbc@gmail.com>
2026-02-18 12:41:19 +0100

    merge design and custom domain migration heads

---
+++ migrations/versions/bbcc42d2f54a_merge_design_and_custom_domain_.py
@@ -0,0 +1,28 @@
+"""merge design and custom domain migrations
+
+Revision ID: bbcc42d2f54a
+Revises: b3f1a2c4d567, b3f1a2c4d5e6
+Create Date: 2026-02-18 12:41:01.957359
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'bbcc42d2f54a'
+down_revision: Union[str, Sequence[str], None] = ('b3f1a2c4d567', 'b3f1a2c4d5e6')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
